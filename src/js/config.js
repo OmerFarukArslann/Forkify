9a0f36b90a39c1271b12f701c87d91310f388728
OmerFarukArslann <ofa5453@gmail.com>
2022-02-28 16:47:36 +0400

    Initital commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'a9a6cb3c-64f8-4ce2-869b-0488a9a072c3';
+export const MODAL_CLOSE_SEC = 2.5;
